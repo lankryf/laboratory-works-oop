dafa18835a31da3b1ae05595e1aea85ac246b638
Oleksii Fedorenko <lankrf@gmail.com>
2024-10-19 08:19:30 +0300

    Add exceptions

--- database/Models/Student.php
+++ database/Models/Student.php
@@ -7,6 +7,16 @@ use Bubblegum\Database\Model;
 class Student extends Model
 {
     protected $tableName = 'students';
+
+
+    public static function create(array $data): Model
+    {
+        if (preg_match('/^[a-zA-Z\s]+$/', $data['name']))
+        {
+            throw new \Exception('Only letters and white space allowed');
+        }
+        return parent::create($data);
+    }
     public function multiplyBigNumbers(int $number1, int $number2): float|int
     {
         return $number1 * $number2;
